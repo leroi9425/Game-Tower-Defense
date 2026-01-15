a3db2415691fcacc95138c455d66a29c5a6b33ba
Minh Hoang <leroi9425@gmail.com>
2026-01-15 15:05:41 +0700

    fix: Sửa lại lỗi nhỏ

--- Assets/Scripts/TowerUpgrageUI.cs
+++ Assets/Scripts/TowerUpgrageUI.cs
@@ -22,5 +22,26 @@ public class TowerUpgrageUI : MonoBehaviour
     }
     private void Update()
     {
+        //// Xử lý để không đóng UI ngay lập tức khi vừa mở
+        //if (justOpened)
+        //{
+        //    justOpened = false;
+        //    return;
+        //}
+
+        //// Đóng UI nếu người chơi click ra ngoài bảng
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    if (EventSystem.current.IsPointerOverGameObject())
+        //        return;
+
+        //    Destroy(gameObject);
+        //}
+
+        //// Tự động đóng nếu tháp đã đạt cấp tối đa
+        //if (Tower.indexUpgradeStage >= tower.upgradeStages.Length)
+        //{
+        //    Destroy(gameObject);
+        //}
     }
 }
